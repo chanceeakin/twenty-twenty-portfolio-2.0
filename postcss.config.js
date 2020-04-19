5ea91159392fb5edbe1f86615a7b77cdcf8c5617
Chance Eakin <chance.eakin@gmail.com>
2020-04-18 20:33:58 -0500

    production optimizations

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,17 @@
 const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.rs"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
+  plugins: [
+    tailwindcss("./tailwind.config.js"),
+    require("autoprefixer"),
+    // hacky way to get a SLIM css file. ain't broke, don't fix it
+    ...(process.argv[3] === "production" ? [purgecss] : []),
+  ],
 };
